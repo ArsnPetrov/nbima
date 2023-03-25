2b090a40dd15201bb24c4f1d15126db45f12cbb9
Arsenij <Arsenij>
2023-03-25 19:38:14 +0300

    GUI_Spectre class. Minor chanes to run on OS X

---
+++ GUI_Spectre.cpp
@@ -0,0 +1,8 @@
+//
+//  GUI_Spectre.cpp
+//  nbima
+//
+//  Created by Arseny on 23.03.2023.
+//
+
+#include "GUI_Spectre.hpp"
---
+++ GUI_Spectre.hpp
@@ -0,0 +1,13 @@
+//
+//  GUI_Spectre.hpp
+//  nbima
+//
+//  Created by Arseny on 23.03.2023.
+//
+
+#ifndef GUI_Spectre_hpp
+#define GUI_Spectre_hpp
+
+#include <stdio.h>
+
+#endif /* GUI_Spectre_hpp */
